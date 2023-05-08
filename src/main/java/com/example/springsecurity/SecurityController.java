58060fa361cbd6acdb435aaff43681a4d225e917
yoonhyuk.jeong <tanibourne@gmail.com>
2023-05-08 18:20:47 +0900

    spring-boot version 2.6.3

---
+++ src/main/java/com/example/springsecurity/SecurityController.java
@@ -0,0 +1,16 @@
+package com.example.springsecurity;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+
+
+    @GetMapping("/")
+    public String index(){
+        return "string";
+
+    }
+}
