0d527bb4710d0d2d69d06237fae888b21f23e3d4
yoonhyuk.jeong <tanibourne@gmail.com>
2023-05-08 18:22:29 +0900

    spring-boot version 2.6.3

--- src/main/java/com/example/springsecurity/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/SecurityConfig.java
@@ -15,5 +15,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
         http // 인증 정책
                 .formLogin();
+        //
     }
 }
